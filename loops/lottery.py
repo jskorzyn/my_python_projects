d58281c7b62c2672c3d38c80a503f22ab59eb5a8
Jacek Skorzynski <jskorzyn@localhost.localdomain>
2020-04-06 21:12:30 +0200

    Lottery Program

---
+++ loops/lottery.py
@@ -0,0 +1,6 @@
+decision = "yes"
+while decision == "yes":
+    decision = input("Would you like to play? Put yes or no: ")
+#    for i in range(6):
+#        i = i+1
+#        input("Podaj liczbe:" int())
