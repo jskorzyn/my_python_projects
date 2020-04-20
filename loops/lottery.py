64d8f0aa3be4fb1ac8433709cdb93fbe51f553d3
Jacek Skorzynski <jskorzyn@localhost.localdomain>
2020-04-20 22:45:04 +0200

    test

--- loops/lottery.py
+++ loops/lottery.py
@@ -1,6 +1,11 @@
+import random
+
 decision = "yes"
 while decision == "yes":
     decision = input("Would you like to play? Put yes or no: ")
-#    for i in range(6):
-#        i = i+1
-#        input("Podaj liczbe:" int())
+    i = 0
+    for i in range(6):
+        i = i+1
+        user_numbers.append = input("Give a number:" )
+        print(user_numbers)
+        print("test")
